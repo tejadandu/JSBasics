dd5a5e1571195638d048348de77e03ff565d18c7
Teja Dandu <teja.dandu484@gmail.com>
2023-01-24 11:35:14 +0530

    added codes

---
+++ Jan_24/datatypesDemo.js
@@ -0,0 +1,24 @@
+var marks = 100;
+var name = 'Teja';
+var age;
+var company = null;
+var salary = false;
+var isStudent = false;
+
+console.log("Marks = ", marks);
+console.log("Name = ", name);
+console.log("age = ", age);
+console.log('Company = ', company);
+console.log('Salary = ', salary);
+console.log("Is a Student ? = ", isStudent);
+
+var user = {name: "Teja", age: 24, company: 'null', isStudent: false};
+var user2 = {
+    name: "dandu",
+    age: 28,
+    company: "MountBlue",
+    isStudent: false
+};
+
+console.log("Details of the user is ", user);
+console.log("another user is ", user2);
---
+++ Jan_24/specialchars.js
@@ -0,0 +1,9 @@
+let text = "The new apple iphone \n has been launched";
+console.log(text);
+let text2 = "The new apple iphone 14 \t has been launched";
+console.log(text2);
+
+
+let age = 24;
+let name = "Teja";
+console.log(name, "\n", age);
