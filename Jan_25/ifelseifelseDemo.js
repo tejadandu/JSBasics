37571ca71aff364f30d68ce8b24353abe62cbc1d
Teja Dandu <teja.dandu484@gmail.com>
2023-01-25 20:35:15 +0530

    Added ifelsedemo

---
+++ Jan_25/ifelseifelseDemo.js
@@ -0,0 +1,14 @@
+let isUserPremium = false;
+let isUserVIP = false;
+let isUserSport = true;
+if(isUserPremium){
+    console.log("Enjoy the premium content on hotstar");
+}
+else if(isUserVIP){
+    console.log("Enjoy the sports content on hotstar");
+}
+else{
+    console.log("Please purchase either VIP or premium subscription on hotstar");
+}
+
+console.log("END");
