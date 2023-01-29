3ec6e7a8d2ff4c32b71e2dc0885d8a85b4a50747
Teja Dandu <teja.dandu484@gmail.com>
2023-01-29 11:06:58 +0530

    Added for and while loops

--- Jan_26/checkTriangle.js
+++ Jan_26/checkTriangle.js
@@ -3,12 +3,23 @@ if sum of its two sides is greater than
 the third side.
 */
 
-let a = 7;
-let b = 10;
-let c = 3;
+let a = 10;
+let b = 4;
+let c = 9;
 
 if(a + b > c && b + c > a && c + a > b){
     console.log("Yes, valid for triangle");
 }else{
     console.log("No, it's a triangle");
 }
+
+
+if(a == b && b == c && c == a){
+    console.log("Equilateral");
+}else if(a==b || b==c || c==a){
+    console.log("isoscelene");
+}else{
+    console.log("scelene");
+}
+
+
---
+++ Jan_29/forDemo1.js
@@ -0,0 +1,4 @@
+// program to print numbers from 1 - 20 using for loop..
+for(let i = 0; i <= 20; i+=1){
+    console.log(i);
+}
