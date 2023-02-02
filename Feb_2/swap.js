b786d87b1e30a692c9664161d646a5dbc35344d2
Teja Dandu <teja.dandu484@gmail.com>
2023-02-02 10:51:51 +0530

    Added swaping variables

---
+++ Feb_2/swap.js
@@ -0,0 +1,7 @@
+// Given two numbers swap them using third variable
+let x = 10;
+let y = 30;
+let temp = x;
+x = y;
+y = temp;
+console.log("Swaping the variables X, Y:", x, y);
---
+++ Feb_2/swapWithoutThirdVariable.js
@@ -0,0 +1,6 @@
+let x = 10;
+let y = 30;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("Swaping the X, Y: ", x, y);
