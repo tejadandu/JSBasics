aaec19f43af760de9e229b4cf4c83e5191f62af7
Teja Dandu <teja.dandu484@gmail.com>
2023-02-05 08:36:01 +0530

    Added pattern

---
+++ Feb_3/patter5.js
@@ -0,0 +1,12 @@
+function pattern(n){
+    for(row = 1; row <= n; row += 1){
+        let str = "";
+        let stars = n - row;
+        for(let col = 1; col <= stars+1; col += 1){
+            str += "*";
+        }
+        console.log(str);
+    }
+}
+
+console.log(pattern(5));
